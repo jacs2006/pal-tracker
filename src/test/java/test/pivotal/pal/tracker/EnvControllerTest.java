052dcfd1f061003131a2fed03f309df9b7d1eff0
Joshua Seonie <jacs2006@yahoo.com>
2018-06-04 15:07:19 -0600

    Deploy a Spring Boot app to Cloud Foundry

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -8,8 +8,10 @@ import io.pivotal.pal.tracker.EnvController;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnvControllerTest {
+
     @Test
     public void getEnv() throws Exception {
+
         EnvController controller = new EnvController(
             "8675",
             "12G",
@@ -24,5 +26,4 @@ public class EnvControllerTest {
         assertThat(env.get("CF_INSTANCE_INDEX")).isEqualTo("34");
         assertThat(env.get("CF_INSTANCE_ADDR")).isEqualTo("123.sesame.street");
     }
-
 }
